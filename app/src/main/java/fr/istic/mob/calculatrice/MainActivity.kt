0fb0b36fbc1d0f8237288e78fa279bb3086d72e7
Bio Paul Kobena <biokobena07@gmail.com>
2025-09-20 12:43:53 +0200

    first interface

---
+++ app/src/main/java/fr/istic/mob/calculatrice/MainActivity.kt
@@ -0,0 +1,29 @@
+package fr.istic.mob.calculatrice
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var textInput : EditText
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        textInput = findViewById(R.id.textInput)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.trois)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+    fun buttonFunction(view: View?) {
+    println()
+    }
+}
